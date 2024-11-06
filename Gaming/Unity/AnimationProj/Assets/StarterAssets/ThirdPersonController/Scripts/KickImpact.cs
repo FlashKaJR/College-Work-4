e2d735c2762e180f6d4224f3a1c8fe0a50805210
FlashKa <stolerusor@gmail.com>
2024-11-06 02:57:36 +0000

    Ragdoll complete
    Fixed weighting issue + player input issue
    Player ragdolls  properly now and stands up in the correct position and cant move while ragdoll.

--- Gaming/Unity/AnimationProj/Assets/StarterAssets/ThirdPersonController/Scripts/KickImpact.cs
+++ Gaming/Unity/AnimationProj/Assets/StarterAssets/ThirdPersonController/Scripts/KickImpact.cs
@@ -1,14 +1,18 @@
+using StarterAssets;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class KickImpact : MonoBehaviour
 {
-       public float pushForce = 10f;
-       public bool push;
-       public bool standUp;
-       public GameObject player;
-       public GameObject playerPosition;
+    public float pushForce = 10f;
+    public float timer = 1f;
+    public bool push;
+    public bool standUp;
+    public GameObject player;
+    public GameObject playerPosition;
+    public InputActionAsset playerInput;
+    public InputActionAsset ragdollInput;
     public Animator playerAnimator;
     public Rigidbody hipsRigidbody;
     public Rigidbody leftUpLegRigidbody;
@@ -25,16 +29,19 @@ public class KickImpact : MonoBehaviour
     public Rigidbody rightArmRigidbody;
     public Rigidbody rightForeArmRigidbody;
     public Rigidbody rightHandRigidbody;
+    public Rigidbody pelvisRoot;
 
-
-
+    bool ragdoll;
+    ThirdPersonController ThirdPersonController;
+    Transform oldHeadPos;
 
     void Update()
     {
         playerPosition.transform.position = hipsRigidbody.transform.position;
         if (push)
         {
-            ActivateKick();
+            StartCoroutine(WaitForOneSecond());
+
             push = false;
         }
         if (standUp)
@@ -42,9 +49,21 @@ public class KickImpact : MonoBehaviour
             DeactivateKick();
             standUp = false;
         }
+        if (ragdoll)
+        {
+            StartCoroutine(StopRagdoll());
+            ragdoll = false;
+        }
     }
     public void ActivateKick()
     {
+        //Change input controller        
+        PlayerInput PlayerInput = player.GetComponent<PlayerInput>();
+        if (PlayerInput != null)
+        {
+            PlayerInput.actions = ragdollInput;
+        }
+        ragdoll = true;
         // Deactivate the animator
         if (playerAnimator != null)
         {
@@ -55,82 +74,110 @@ public class KickImpact : MonoBehaviour
         if (hipsRigidbody != null)
         {
             hipsRigidbody.isKinematic = false;
-            hipsRigidbody.AddForce(Vector3.forward * pushForce, ForceMode.Impulse);
+            hipsRigidbody.AddForce(Vector3.back * pushForce, ForceMode.Impulse);
+            hipsRigidbody.AddForce(Vector3.up * pushForce, ForceMode.Impulse);
         }
         if (leftUpLegRigidbody != null)
         {
             leftUpLegRigidbody.isKinematic = false;
-            leftUpLegRigidbody.AddForce(Vector3.forward * pushForce, ForceMode.Impulse);
+            leftUpLegRigidbody.AddForce(Vector3.back * pushForce, ForceMode.Impulse);
+            leftArmRigidbody.AddForce(Vector3.up * pushForce, ForceMode.Impulse);
         }
         if (leftLegRigidbody != null)
         {
             leftLegRigidbody.isKinematic = false;
-            leftLegRigidbody.AddForce(Vector3.forward * pushForce, ForceMode.Impulse);
+            leftLegRigidbody.AddForce(Vector3.back * pushForce, ForceMode.Impulse);
+            leftLegRigidbody.AddForce(Vector3.up * pushForce, ForceMode.Impulse);
         }
         if (leftFootRigidbody != null)
         {
             leftFootRigidbody.isKinematic = false;
-            leftFootRigidbody.AddForce(Vector3.forward * pushForce, ForceMode.Impulse);
+            leftFootRigidbody.AddForce(Vector3.back * pushForce, ForceMode.Impulse);
+            leftFootRigidbody.AddForce(Vector3.up * pushForce, ForceMode.Impulse);
         }
         if (rightUpLegRigidbody != null)
         {
             rightUpLegRigidbody.isKinematic = false;
-            rightUpLegRigidbody.AddForce(Vector3.forward * pushForce, ForceMode.Impulse);
+            rightUpLegRigidbody.AddForce(Vector3.back * pushForce, ForceMode.Impulse);
+            rightUpLegRigidbody.AddForce(Vector3.up * pushForce, ForceMode.Impulse);
         }
         if (rightLegRigidbody != null)
         {
             rightLegRigidbody.isKinematic = false;
-            rightLegRigidbody.AddForce(Vector3.forward * pushForce, ForceMode.Impulse);
+            rightLegRigidbody.AddForce(Vector3.back * pushForce, ForceMode.Impulse);
+            rightLegRigidbody.AddForce(Vector3.up * pushForce, ForceMode.Impulse);
         }
         if (rightFootRigidbody != null)
         {
             rightFootRigidbody.isKinematic = false;
-            rightFootRigidbody.AddForce(Vector3.forward * pushForce, ForceMode.Impulse);
+            rightFootRigidbody.AddForce(Vector3.back * pushForce, ForceMode.Impulse);
+            rightFootRigidbody.AddForce(Vector3.up * pushForce, ForceMode.Impulse);
         }
         if (spineRigidbody != null)
         {
             spineRigidbody.isKinematic = false;
-            spineRigidbody.AddForce(Vector3.forward * pushForce, ForceMode.Impulse);
+            spineRigidbody.AddForce(Vector3.back * pushForce, ForceMode.Impulse);
+            spineRigidbody.AddForce(Vector3.up * pushForce, ForceMode.Impulse);
         }
         if (leftArmRigidbody != null)
         {
             leftArmRigidbody.isKinematic = false;
-            leftArmRigidbody.AddForce(Vector3.forward * pushForce, ForceMode.Impulse);
+            leftArmRigidbody.AddForce(Vector3.back * pushForce, ForceMode.Impulse);
+            leftArmRigidbody.AddForce(Vector3.up * pushForce, ForceMode.Impulse);
         }
         if (leftForeArmRigidbody != null)
         {
             leftForeArmRigidbody.isKinematic = false;
-            leftForeArmRigidbody.AddForce(Vector3.forward * pushForce, ForceMode.Impulse);
+            leftForeArmRigidbody.AddForce(Vector3.back * pushForce, ForceMode.Impulse);
+            leftForeArmRigidbody.AddForce(Vector3.up * pushForce, ForceMode.Impulse);
         }
         if (leftHandRigidbody != null)
         {
             leftHandRigidbody.isKinematic = false;
-            leftHandRigidbody.AddForce(Vector3.forward * pushForce, ForceMode.Impulse);
+            leftHandRigidbody.AddForce(Vector3.back * pushForce, ForceMode.Impulse);
+            leftHandRigidbody.AddForce(Vector3.up * pushForce, ForceMode.Impulse);
         }
         if (headRigidbody != null)
         {
             headRigidbody.isKinematic = false;
-            headRigidbody.AddForce(Vector3.forward * pushForce, ForceMode.Impulse);
+            headRigidbody.AddForce(Vector3.back * pushForce, ForceMode.Impulse);
+            headRigidbody.AddForce(Vector3.up * pushForce, ForceMode.Impulse);
         }
         if (rightArmRigidbody != null)
         {
             rightArmRigidbody.isKinematic = false;
-            rightArmRigidbody.AddForce(Vector3.forward * pushForce, ForceMode.Impulse);
+            rightArmRigidbody.AddForce(Vector3.back * pushForce, ForceMode.Impulse);
+            rightArmRigidbody.AddForce(Vector3.up * pushForce, ForceMode.Impulse);
         }
         if (rightForeArmRigidbody != null)
         {
             rightForeArmRigidbody.isKinematic = false;
-            rightForeArmRigidbody.AddForce(Vector3.forward * pushForce, ForceMode.Impulse);
+            rightForeArmRigidbody.AddForce(Vector3.back * pushForce, ForceMode.Impulse);
+            rightForeArmRigidbody.AddForce(Vector3.up * pushForce, ForceMode.Impulse);
         }
         if (rightHandRigidbody != null)
         {
             rightHandRigidbody.isKinematic = false;
-            rightHandRigidbody.AddForce(Vector3.forward * pushForce, ForceMode.Impulse);
+            rightHandRigidbody.AddForce(Vector3.back * pushForce, ForceMode.Impulse);
+            rightHandRigidbody.AddForce(Vector3.up * pushForce, ForceMode.Impulse);
+        }
+        if (pelvisRoot != null)
+        {
+            pelvisRoot.isKinematic = false;
+            pelvisRoot.AddForce(Vector3.back * pushForce, ForceMode.Impulse);
+            pelvisRoot.AddForce(Vector3.up * pushForce, ForceMode.Impulse);
         }
     }
     //Activate anmimator and kinematic rigidbodies
     public void DeactivateKick()
     {
+        //Change input controller        
+        PlayerInput PlayerInput = player.GetComponent<PlayerInput>();
+        if (PlayerInput != null)
+        {
+            PlayerInput.actions = playerInput;
+        }
+        ragdoll = false;
         TeleportPlayer();
         // Activate the animator
         if (playerAnimator != null)
@@ -199,12 +246,31 @@ public class KickImpact : MonoBehaviour
         {
             rightHandRigidbody.isKinematic = true;
         }
+        if (pelvisRoot != null)
+        {
+            pelvisRoot.isKinematic = true;
+        }
     }
     //Teleport player to rigidbody position
     public void TeleportPlayer()
     {
-        Debug.Log("Teleporting player");
-        player.transform.position += playerPosition.transform.position;
+        player.transform.position = playerPosition.transform.position + new Vector3(0, 0.2f, 0);
     }
-  
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            push = true;
+        }
+    }
+    private IEnumerator WaitForOneSecond()
+    {
+        yield return new WaitForSeconds(timer);
+        ActivateKick();
+    }
+    private IEnumerator StopRagdoll()
+    {
+        yield return new WaitForSeconds(4f);
+        DeactivateKick();
+    }
 }
